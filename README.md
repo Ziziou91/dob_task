fe91d7a18e8768bc395b6b30479ebb9ef3901eb2
Josh Gray <joshgray91@gmail.com>
2024-01-29 20:54:06 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# dob_task
+
+Simple application to print out names and dates of birth from 'DOB.txt'
+
+## Usage
+
+To run the application from the terminal enter
+
+`python dob_task.py`
---
+++ dob_task.py
@@ -0,0 +1,11 @@
+"""Simple application to print out names and dates of birth from 'DOB.txt'"""
+print(f"{'*'*69}")
+print(f"{'='*29}dob_task.py{'='*29}")
+print(f"{'*'*69}\n")
+
+print("\nprint out will go here\n")
+
+print(f"{'*'*69}")
+print(f"{'='*27}dob_task.py END{'='*27}")
+print(f"{'*'*69}\n")
+
