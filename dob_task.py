e2f555a8f3a292e365eb7769315f7aadc2b0e1ac
Josh Gray <joshgray91@gmail.com>
2024-01-31 19:43:51 +0000

    implement error handling for file open

--- dob_task.py
+++ dob_task.py
@@ -1,20 +1,22 @@
 """Simple application to print out names and dates of birth from 'DOB.txt'."""
-file_name = "./DOB.txt"
+file_name = "DOB.txt"
 
 def app() -> None:
     """Reads DOB.txt file and organises the data before using it to call print_person."""
     name_dob_list = []
-    with open(file_name, "r", encoding="UTF-8") as file:
-        for line in file:
-            person = line[:-1].split(" ", 2)
-            new_line = [" ".join(person[:2])]+person[2:]
-            name_dob_list.append(new_line)
-                       
+    try:
+        file = open(file_name, "r", encoding="UTF-8")
+    except FileNotFoundError:
+        print(f"error, {file_name} is an invalid path.")
+    else:
+        with file:
+            for line in file:
+                person = line[:-1].split(" ", 2)
+                new_line = [" ".join(person[:2])]+person[2:]
+                name_dob_list.append(new_line)                 
     print_person(name_dob_list, "Name")
     print("\n")
     print_person(name_dob_list, "Birthdate")
-
-
 
 def print_person(input_list: list, input_type: str) -> None:
     """Loops through the given list and prints string depending on a given input_type string."""
@@ -25,12 +27,16 @@ def print_person(input_list: list, input_type: str) -> None:
         else:
             print(val[1])
 
-print(f"{'*'*69}")
+def print_line(char="-", count=69):
+    """Print a line of 'char' with 'count' occurances."""
+    print(f"{char*count}")
+
+print_line(char="*")
 print(f"{'='*29}dob_task.py{'='*29}")
-print(f"{'*'*69}\n")
+print_line(char="*")
 
 app()
 
-print(f"\n{'*'*69}")
+print_line(char="*")
 print(f"{'='*27}dob_task.py END{'='*27}")
-print(f"{'*'*69}\n")
+print_line(char="*")
